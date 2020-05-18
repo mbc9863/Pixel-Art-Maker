6e6cb9f4cb12037b4c6da51a897138a6f3d803c7
mbc9863 <46851311+mbc9863@users.noreply.github.com>
2020-05-18 08:10:22 -0700

    feat: More indentation fix

--- designs.js
+++ designs.js
@@ -4,30 +4,30 @@ const $inputWidth = $('#inputWidth');
 const $colorPicker = $('#colorPicker');
 
 // Select size input
-$('#sizePicker').submit( event => {
+$('#sizePicker').submit(event => {
     event.preventDefault();
     let width = $inputWidth.val();
     let height = $inputHeight.val();
 
 // Clear
 // When size is submitted by the user, call makeGrid()
-$tableElement.html('');
+    $tableElement.html('');
     makeGrid(height, width);
     addCellClickListener();
 });
 
 function makeGrid(height, width) {
-    for(let i = 0; i < height; i++) {
+    for (let i = 0; i < height; i++) {
         $tableElement.append('<tr></tr>');
     };
-	for(let i = 0; i < width; i++) {
+    for (let i = 0; i < width; i++) {
         $('tr').append('<td></td>');
     };
 };
 
 // Select color input
 function addCellClickListener() {
-    $('td').click( event => {
+    $('td').click(event => {
         let color = $colorPicker.val();
         $(event.currentTarget).css("background-color", color)
     });
